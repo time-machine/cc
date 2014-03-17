5d09413f6f7d45fe4154b2c3742baa721d869969
Tery Lim <kumabotz@gmail.com>
2014-03-17 23:41:34 +0800

    Add more configs

--- server_config.js
+++ server_config.js
@@ -1,5 +1,19 @@
 var config = {};
 
+config.unittest = process.argv.indexOf('--unittest') > -1;
+
 config.port = process.env.COCO_PORT || process.env.COCO_NODE_PORT || 3000;
+
+config.mongo = {};
+config.mongo.host = process.env.COCO_MONGO_HOST || 'localhost';
+
+config.cookie_secret = process.env.COCO_COOKIE_SECRET || 'chips ahoy';
+
+config.isProduction = config.mongo.host !== 'localhost';
+
+if (!config.unittest && !config.isProduction) {
+  // change artificially slow down non-static requests for testing
+  config.slow_down = false;
+}
 
 module.exports = config;
